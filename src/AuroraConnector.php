0114c792aa517bed9b571789296c254d46304e31
Akash Sharma <macccostello@gmail.com>
2022-11-02 23:57:46 +0530

    Add ReadConsistency

--- src/AuroraConnector.php
+++ src/AuroraConnector.php
@@ -19,26 +19,6 @@ final class AuroraConnector extends MySqlConnector
         if (! empty($config['password'])) {
             return parent::createConnection($dsn, $config, $options);
         }
-
-        // $execute = function (int $attempt) use ($dsn, $config, $options) {
-        //     if (! isset($config['aurora']['secret'])) {
-        //         throw new Exception('The secret name must be defined on database.{connection}.aurora.secret');
-        //     }
-
-        //     // The Password Resolver extension will keep a cache of the password.
-        //     // If Laravel throws an exception because of wrong password, then
-        //     // we can retry but ask the extension to refresh the cache.
-        //     $refreshCache = $attempt > 1;
-
-        //     $config['password'] = $this->resolver->resolve($config['aurora']['secret'], $refreshCache);
-            
-
-        //     return parent::createConnection($dsn, $config, $options);
-        // };
-
-        // $condition = fn (Exception $e) => $e instanceof PDOException && str_contains($e->getMessage(), 'Access denied for user');
-
-        // return retry(when: $condition, callback: $execute, times: 3);
     }
 
     /**
@@ -57,4 +37,8 @@ final class AuroraConnector extends MySqlConnector
         }
 
     }
+
+
+
+    
 }
